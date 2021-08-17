c571b5a444fe09647a8b347622751b6d18466949
olepunchy <74633934+olepunchy@users.noreply.github.com>
2021-08-17 09:43:58 -0500

    Refactoring the Challenges section

---
+++ Challenges/DotNet/TacoCat/PalindromeModel.cs
@@ -0,0 +1,8 @@
+namespace olepunchy.Challenges.DotNet.TacoCat {
+    public class PalindromeModel {
+        public string InputWord { get; set; }
+        public string ReversedWord { get; set; }
+        public bool IsPalindrome { get; set; }
+        public string Message { get; set; }
+    }
+}
