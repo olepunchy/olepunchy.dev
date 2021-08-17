a2b643c3826bf5d388a7e47fb4eeb87a1928de30
olepunchy <74633934+olepunchy@users.noreply.github.com>
2021-08-17 14:42:37 -0500

    Finishing TacoCat page

--- Challenges/DotNet/TacoCat/PalindromeModel.cs
+++ Challenges/DotNet/TacoCat/PalindromeModel.cs
@@ -1,5 +1,9 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace olepunchy.Challenges.DotNet.TacoCat {
     public class PalindromeModel {
+        [Required]
+        [RegularExpression("^[a-zA-Z ,.:!?-]{2,256}$", ErrorMessage = "Please use letters and punctuation only.")]
         public string Input { get; set; }
         public string Reversed { get; set; }
         public bool IsPalindrome { get; set; }
