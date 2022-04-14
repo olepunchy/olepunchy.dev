8a7195841cadcb2553f0733f1f980a1cfdb7b49d
Jeremy Novak <74633934+olepunchy@users.noreply.github.com>
2022-04-14 08:24:43 -0500

    Updated
    Updated

--- Models/MailSettings.cs
+++ Models/MailSettings.cs
@@ -1,12 +1,12 @@
 namespace olepunchy.Models {
     public class MailSettings {
-        public string ToAddress { get; set; } = "olepunchy@gmail.com";
+        public string ToAddress { get; set; }
         public string FromAddress { get; set; }
         public string Subject { get; set; }
         public string Body { get; set; }
-        public string MailServer { get; } = "smtp.gmail.com";
-        public int MailPort { get; } = 587;
-        public string MailPassword { get; } = "naiybfivefnkymyi";
+        public string MailServer { get; }
+        public int MailPort { get; }
+        public string MailPassword { get; }
 
     }
 }
